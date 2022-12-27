71e0b0c7e9ceb25265e228c2be643552a34b518a
davidgfb <davidgracia99@gmail.com>
2022-12-27 11:57:30 +0100

    Update ST IM FS.glsl

--- ST IM FS.glsl
+++ ST IM FS.glsl
@@ -33,6 +33,10 @@ vec2 nearest(vec2 c1, vec2 c2, vec2 c3) { //hay 1 pto fuga
     return value;
 }
 
+vec3 get_Tex2(vec2 v) { 
+    return texture(iChannel0, fract(v)).xyz; 
+}
+
 vec3 render(in vec2 tID, in vec3 ro, in vec3 rd, in vec3 size) {
     float t = tID.x, id = tID.y;  
     vec3 color1 = vec3(0.42, 0.85, 0.65), color2 = vec3(1.0, 0.67, 0.55), color3 = vec3(1.0, 0.9, 0.49), 
@@ -40,20 +44,16 @@ vec3 render(in vec2 tID, in vec3 ro, in vec3 rd, in vec3 size) {
          pos = (ro + t * rd) / size, tex = vec3(0), value = vec3(0); //green, top //red, bottom //yellow, right //orange, left //blue, front 
          //purple, back    
               
-    if (0.9 < id && id < 1.1) {
-        tex = texture(iChannel0, fract(vec2(pos.x, -pos.z))).xyz; 
-        
-        value = color1 * tex;        
+    if (0.9 < id && id < 1.1) {       
+        value = color1 * get_Tex2(vec2(pos.x, -pos.z));        
     
     } else if (1.9 < id && id < 2.1) { 
         tex = texture(iChannel0, fract(pos.xz)).xyz; 
         
         value = color2 * tex;
     
-    } else if (2.9 < id && id < 3.1) { 
-        tex = texture(iChannel0, fract(vec2(-pos.z, pos.y))).xyz; 
-        
-        value = color3 * tex;
+    } else if (2.9 < id && id < 3.1) {      
+        value = color3 * get_Tex2(vec2(-pos.z, pos.y));
     
     } else if (3.9 < id && id < 4.1) { 
         tex = texture(iChannel0, fract(pos.zy)).xyz; 
@@ -74,19 +74,16 @@ vec3 render(in vec2 tID, in vec3 ro, in vec3 rd, in vec3 size) {
     return value;
 }
 
-void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec3 cameraPos = vec3(sin(iTime), cos(iTime) / 2.0, 0);    
-    
-    vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
-    vec3 ro = vec3(uv, 2.14), rd = normalize(ro - cameraPos), size = vec3(0.8, 0.6, 1), up = vec3(0, 1, 0), 
-         right = vec3(1, 0, 0), front = vec3(0, 0, 1); //tiene que ser 2.1...2.19
-    
-    // intersect
-    vec2 tID1 = intersect(ro, rd, up, size.y, 1.0, 2.0), tID2 = intersect(ro, rd, right, size.x, 3.0, 4.0),
-         tID3 = intersect(ro, rd, front, size.z, 5.0, 6.0), tID = nearest(tID1, tID2, tID3);
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {   
+    vec3 cameraPos = vec3(sin(iTime), cos(iTime) / 2.0, 0), 
+         ro = vec3((fragCoord * 2.0 - iResolution.xy) / iResolution.y, 2.14), rd = normalize(ro - cameraPos),
+         size = vec3(0.8, 0.6, 1), up = vec3(0, 1, 0), right = vec3(1, 0, 0), front = vec3(0, 0, 1); 
+         //tiene que ser 2.1...2.19 //uv
     
     // render 
-    vec3 color = render(tID, ro, rd, size);
+    vec3 color = render(nearest(intersect(ro, rd, up, size.y, 1.0, 2.0), 
+         intersect(ro, rd, right, size.x, 3.0, 4.0), intersect(ro, rd, front, size.z, 5.0, 6.0)), ro, rd, size);
+         //clave // intersect //tID1...3
 
     // Output to screen
     fragColor = vec4(color, 1);
